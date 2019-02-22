8a4ca1a8648a020c58a7a5662de07b503d52912b
pqoalla <pqoalla@gmail.com>
2019-02-22 19:04:19 +0400

    ADO.NET Exemples

---
+++ ADO.NETExenples/ADO.NETExenples/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+namespace ADO.NETExenples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             string Connect= @" Data Source =(localdb)\MSSQLLocalDB; Initial Catalog = Fifadb; Integrated Security = True";
+            using (SqlConnection sqlConnection=new SqlConnection (Connect))
+            {
+                try
+                {
+                    sqlConnection.Open();
+                    Console.WriteLine(sqlConnection.State);
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+
+        }
+    }
+}
